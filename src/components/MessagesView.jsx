9f1aa6709135e4a148cf526f615eff0b30effbab
khachaturA <khach.obt@gmail.com>
2020-05-21 13:21:12 -0400

    message UI updated

--- src/components/MessagesView.jsx
+++ src/components/MessagesView.jsx
@@ -3,26 +3,68 @@ import "./MessagesView.css";
 import messageImg from "./img/message.png";
 
 class MessagesView extends Component {
+  constructor(){
+    super();
+    this.state = {
+      hasMsgs:true
+    }
+  }
+  
   render() {
+    const myId = 17;
+    const messages = this.props.msgsList.results;
     this.props.sendMsg("msg");
     return (
-      <div className="chat-empty">
-        <div className="rectangle-copy ">
-          <div className="start-messaging-agen">
-            Start messaging agents, sellers, or even your friends
-          </div>
-          <div className="rectangle">
-            <input
-              placeholder="Write a message"
-              type="text"
-              className="write-a-message"
-            />
+      <React.Fragment>
+        <React.Fragment>
+        {   
+          !this.state.hasMsgs && 
+          <div className="chat-empty">
+            <div className="rectangle-copy ">
+              <div className="start-messaging-agen">
+                Start messaging agents, sellers, or even your friends
+              </div>
+              <div className="rectangle">
+                <input
+                  placeholder="Write a message"
+                  type="text"
+                  className="write-a-message"
+                />
 
-            <img src={messageImg} className="message" />
+                <img src={messageImg} className="message" />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    );
+
+    }
+        </React.Fragment>
+        <React.Fragment>
+        {   
+            this.state.hasMsgs && 
+          <div className="chat-full">
+            <div className="rectangle-copy ">
+              <div className="start-messaging-agen">
+                {messages.map((msg)=>{
+                  return <div className={msg.id===myId?"my-msg":"their-msg"}>{msg.content}</div>
+                })}
+              </div>
+              <div className="rectangle">
+                <input
+                  placeholder="Write a message"
+                  type="text"
+                  className="write-a-message"
+                />
+  
+                <img src={messageImg} className="message" />
+              </div>
+            </div>
+          </div>
+  
+        }
+        </React.Fragment>
+      </React.Fragment>
+     );
+    
   }
 }
 
